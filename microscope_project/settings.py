778560f53496077df95543ed5f1dc7d68498536f
Oluwademiladeogo <bickerstethdemilade@gmail.com>
2025-04-11 23:34:34 +0100

    fix: debug error in production

--- microscope_project/settings.py
+++ microscope_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False").lower() == "false"
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = ["*"]
 
