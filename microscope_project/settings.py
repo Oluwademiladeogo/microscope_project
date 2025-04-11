576157a34292d77c20da6bd5928b4b68a0545ca0
Oluwademiladeogo <bickerstethdemilade@gmail.com>
2025-04-11 22:53:18 +0100

    fix: update secret key from Django's default and store in env

--- microscope_project/settings.py
+++ microscope_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s75$q5#=)hq34o_7to9%=2wl#=1_rvnpm=31t_%x_5rr0isjly'
+SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
